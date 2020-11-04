51c451b28b075e24266397f8c0abc1c5f3e89351
Yu Gao <gaoyububest@gmail.com>
2020-11-04 23:51:06 +0000

    Game all set for play!

---
+++ constants.js
@@ -0,0 +1,23 @@
+const IP = '135.23.222.131';
+const PORT = 50542;
+
+const MOVE_UP_KEY = 'w';
+const MOVE_DOWN_KEY = 's';
+const MOVE_LEFT_KEY = 'a';
+const MOVE_RIGHT_KEY = 'd';
+
+const MESSAGE = {
+  h: `Say: Hi, everyone!`,
+  b: `Say: Goodbye!`, 
+  l: `Say: I love it!`
+};
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_RIGHT_KEY,
+  MESSAGE
+};
--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+const { MOVE_UP_KEY, MOVE_DOWN_KEY, MOVE_LEFT_KEY, MOVE_RIGHT_KEY, MESSAGE } = require('./constants');
+
 let connection;
 
 const setupInput = function(conn) {
@@ -14,14 +16,19 @@ const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
   }
-  if (key === 'w') {
+
+  if (key === MOVE_UP_KEY) {
     connection.write('Move: up');
-  } else if (key === 's') {
+  } else if (key === MOVE_DOWN_KEY) {
     connection.write('Move: down');
-  } else if (key === 'a') {
+  } else if (key === MOVE_LEFT_KEY) {
     connection.write('Move: left');
-  } else if (key === 'd') {
+  } else if (key === MOVE_RIGHT_KEY) {
     connection.write('Move: right');
+  } else {
+    if (MESSAGE[key]) {
+      connection.write(MESSAGE[key]);
+    }
   }
 };
 
