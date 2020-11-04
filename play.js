ac25e99e44b037a84778cb021d0c8ba63c9a5703
Yu Gao <gaoyububest@gmail.com>
2020-11-04 19:40:10 +0000

    separate connection module created

--- play.js
+++ play.js
@@ -1,35 +1,3 @@
-const net = require('net');
-
-const listOfClients = []
-
-const addClientToList = (client) => {
-  listOfClients.push(client)
-}
-
-const generateRandomId = () => Math.floor(Math.random() * 2000)
-
-const broadcast = (listOfClients, message, id) => {
-  console.log(id)
-  for (const client of listOfClients) {
-    if (client.id !== id) {
-      client.write(message)
-    }
-  }
-}
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '135.23.222.131',
-    port: 50542
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on('data', data => console.log('Server says: ', data));
-  return conn;
-}
-
+const connect = require('./client');
 console.log('Connecting ...');
 connect();
